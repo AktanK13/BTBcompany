c4abd64150c1c9a0b96153835c4d6fb9a9f7f84c
Undefind <aktan@MacBook-Air-Aktan.local>
2023-08-25 22:17:49 +0600

    add translate

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import { useState } from 'react';
 import './sidebar.css'
 import { slide as Menu } from "react-burger-menu";
+import { useTranslation } from 'react-i18next';
 
 const Sidebar = (props) => {
+    const { t, i18n } = useTranslation();
+    const changeLanguage = (language) => {
+        i18n.changeLanguage(language);
+    };
+    const [isOpen, setIsOpen] = useState(false);
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
+    console.log(isOpen, 'isOpen');
     return (
-        <Menu {...props} >
-            <a className="menu-item" href="#aboutus">
-                О нас
+        <Menu {...props} isOpen={isOpen} >
+            <a className='menu-items' href="#aboutus" onClick={closeMenu}>
+                {t("about")}
             </a>
 
-            <a className="menu-item" href="#products">
-                Продукция
+            <a className='menu-items' href="#products" onClick={closeMenu}>
+                {t("products")}
             </a>
-            <a className="menu-item" href="#price">
-                Цены
+            <a className='menu-items' href="#price" onClick={closeMenu}>
+                {t("prices")}
             </a>
 
-            <a className="menu-item" href="#contacts">
-                Контакты
+            <a className='menu-items' href="#contacts" onClick={closeMenu}>
+                {t("contacts")}
             </a>
+            {
+                i18n.language === 'ru' ?
+                    <button
+                        className=" menu-item btn text-xs p-2 bg-gray-800 w-full sm:w-auto sm:ml-1"
+                        onClick={() => changeLanguage("kg")}>KG</button>
+                    :
+                    <button
+                        className=" menu-item btn text-xs p-2 bg-gray-800 w-full sm:w-auto sm:ml-1"
+                        onClick={() => changeLanguage("ru")}>RU</button>
+            }
         </Menu>
     )
 }
